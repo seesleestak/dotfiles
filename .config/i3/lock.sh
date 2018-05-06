2fc8e0466b14d6958277198e09c21b2c04cdbca4
Calvin Cieslak <calvin.cieslak@hixme.com>
2018-05-06 08:50:48 -0700

    i3 conf updates

---
+++ .config/i3/lock.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Take a screenshot:
+SCREENSHOT=/tmp/screen.png
+scrot $SCREENSHOT
+
+# Lock image and blur type variables
+LOCK_IMAGE=~/.config/i3/lock.png
+BLURTYPE="0x4"
+
+# Create a blur on the shot:
+convert $SCREENSHOT -blur $BLURTYPE $SCREENSHOT
+
+# Add the lock to the blurred image:
+[[ -f ~/.config/i3/lock.png ]] && convert $SCREENSHOT $LOCK_IMAGE -gravity center -composite -matte $SCREENSHOT
+
+# Pause music (mocp and mpd):
+# mocp -P
+# mpc pause
+
+# Lock it up!
+i3lock -e -f -c 000000 -i $SCREENSHOT
+
+# If still locked after 20 seconds, turn off screen.
+# sleep 20 && pgrep i3lock && xset dpms force off
