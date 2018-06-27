4484210b564fea4511022d54d696633d501e6dc3
Calvin Cieslak <calvin.cieslak@gmail.com>
2018-06-27 10:39:58 -0700

    Clean up scripts

--- scripts/.scripts/monitor-built-in.sh
+++ scripts/.scripts/monitor-built-in.sh
@@ -1,4 +1,2 @@
 # !/bin/sh
 xrandr --output VGA1 --off --output HDMI1 --off --output LVDS1 --auto && i3-msg restart
-
-
--- scripts/.scripts/quit-all.sh
+++ scripts/.scripts/quit-all.sh
@@ -1,7 +1,12 @@
 # !/bin/sh
+
+# Kill all commonly used programs
 killall slack
 killall chromium
 killall spotify
 killall urxvt
 killall telegram-desktop
 killall libreoffice
+
+# Go to first workspace
+i3-msg workspace "1"
