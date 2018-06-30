8a724231ce50cf5645615f732657ae4bb4a5cc57
Calvin Cieslak <calvin.cieslak@gmail.com>
2018-06-30 15:16:19 -0700

    Update scripts for screenshots

---
+++ scripts/.scripts/clip.sh
@@ -0,0 +1,2 @@
+# !/bin/sh
+import clip.png && mv clip.png /home/calvin/Pictures/screenshots && notify-send 'Screenshot taken'
--- scripts/.scripts/screenshot.sh
+++ scripts/.scripts/screenshot.sh
@@ -1,2 +1,2 @@
 # !/bin/sh
-scrot -d 1 screenshot-$(date -u '+%Y-%m-%d-%H:%M:%S').png && notify-send 'Screenshot taken'
+scrot -d 1 /home/calvin/Pictures/screenshots/screenshot-$(date -u '+%Y-%m-%d-%H:%M:%S').png && notify-send 'Screenshot taken'
