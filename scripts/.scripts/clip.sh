4b827af5371f012ace23e1e432144ba464db8f04
Calvin Cieslak <calvin.cieslak@gmail.com>
2018-07-02 08:58:15 -0700

    Generate date with clip screenshots

--- scripts/.scripts/clip.sh
+++ scripts/.scripts/clip.sh
@@ -1,2 +1,3 @@
 # !/bin/sh
-import clip.png && mv clip.png /home/calvin/Pictures/screenshots && notify-send 'Screenshot taken'
+FILE_NAME=clip-$(date -u '+%Y-%m-%d-%H:%M:%S').png
+import $FILE_NAME && mv $FILE_NAME /home/calvin/Pictures/screenshots && notify-send 'Screenshot taken'
