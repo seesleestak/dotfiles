17f808947e37c2560d579d969764a87f38ea3542
Calvin Cieslak <calvin.cieslak@gmail.com>
2018-07-26 16:31:35 -0700

    Add multi monitor support on polybar

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -7,6 +7,13 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch base bar
-polybar top
+# polybar top
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload top &
+  done
+else
+  polybar --reload top &
+fi
 
 echo "Bar launched..."
