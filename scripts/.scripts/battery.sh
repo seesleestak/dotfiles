7d19e60c6fedbd1aba28651848dff5d8b0c4debd
Calvin Cieslak <calvin.cieslak@gmail.com>
2018-09-10 10:48:32 -0700

    Comment out unused code in battery script

--- scripts/.scripts/battery.sh
+++ scripts/.scripts/battery.sh
@@ -1,9 +1,9 @@
 # !/bin/sh
 COUNT=$(acpi -b | wc -l)
-WEIGHT_1=$(acpi -bi | grep -E -o '[0-9]{1,4} mAh' | tr -d ' mAh' | sed g\;n | sed /^$/d | head -1)
-WEIGHT_2=$(acpi -bi | grep -E -o '[0-9]{1,4} mAh' | tr -d ' mAh' | sed g\;n | sed /^$/d | tail -1)
-VAL_1=$(acpi -b | egrep -o '[0-9]{1,3}%' | tr -d '%' | head -1)
-VAL_2=$(acpi -b | egrep -o '[0-9]{1,3}%' | tr -d '%' | tail -1)
+# WEIGHT_1=$(acpi -bi | grep -E -o '[0-9]{1,4} mAh' | tr -d ' mAh' | sed g\;n | sed /^$/d | head -1)
+# WEIGHT_2=$(acpi -bi | grep -E -o '[0-9]{1,4} mAh' | tr -d ' mAh' | sed g\;n | sed /^$/d | tail -1)
+# VAL_1=$(acpi -b | egrep -o '[0-9]{1,3}%' | tr -d '%' | head -1)
+# VAL_2=$(acpi -b | egrep -o '[0-9]{1,3}%' | tr -d '%' | tail -1)
 
 SUM=$(acpi -b | egrep -o '[0-9]{1,3}%' | tr -d '%' | xargs -I% echo -n '+%')
 NUM=$((SUM/COUNT))
