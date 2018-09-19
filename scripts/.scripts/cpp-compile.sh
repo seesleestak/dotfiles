0bb39aeb93c3412cbe06310498e6d95f077e89bf
Calvin Cieslak <calvin.cieslak@gmail.com>
2018-09-19 14:01:34 -0700

    Add better loggin to cpp-compile script

--- scripts/.scripts/cpp-compile.sh
+++ scripts/.scripts/cpp-compile.sh
@@ -1,12 +1,20 @@
 # !/bin/sh
-CURRENT_DIR=$(pwd)
 SOURCE_FILES=()
 
-for i in $CURRENT_DIR/*; do
+# Loop over files in current directory and push to array
+for i in $(pwd)/*; do
   if [[ $i == *.cpp ]]; then
     SOURCE_FILES=("${SOURCE_FILES[@]}" "$i")
   fi
 done
+
+# Logging
+echo "Compiling:"
+for k in ${SOURCE_FILES[@]}; do
+  echo $k
+done
+echo "============================================="
+printf "\n"
 
 # Compile source files and run
 g++ -o prog ${SOURCE_FILES[@]} && ./prog
