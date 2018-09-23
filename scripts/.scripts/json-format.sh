5888885e9c7305233a6d00b97815b234eb51cfaa
Calvin Cieslak <calvin.cieslak@gmail.com>
2018-09-23 12:38:31 -0700

    Simplify xsel flags

--- scripts/.scripts/json-format.sh
+++ scripts/.scripts/json-format.sh
@@ -1,2 +1,2 @@
 # !/bin/sh
-xsel -o -b | python -c 'import json, sys; print(json.dumps(json.load(sys.stdin), indent=2))' | xsel -i -b
+xsel -ob | python -c 'import json, sys; print(json.dumps(json.load(sys.stdin), indent=2))' | xsel -ib
