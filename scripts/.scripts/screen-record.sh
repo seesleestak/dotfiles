1d972b68e072dd184c28f954d9b719e4fb2c42c1
Calvin Cieslak <calvin.cieslak@gmail.com>
2018-09-23 18:40:13 -0700

    Finally work out screen record script with slop

--- scripts/.scripts/screen-record.sh
+++ scripts/.scripts/screen-record.sh
@@ -1,11 +1,10 @@
 # !/bin/bash
-FILE_NAME=screen-record-$(date -u '+%Y-%m-%d-%H:%M:%S').mp4
-# SLOP=$(slop -f "%x %y %w %h %g %i") || exit 1
-# read -r X Y W H G ID < <(echo $SLOP)
-# ffmpeg -f x11grab -s "$W"x"$H" -i :0.0+$X,$Y -framerate 60 /home/calvin/Videos/screen-record/${FILE_NAME}
-ffmpeg \
-  -f x11grab \
-  -s $(xdpyinfo | grep dimensions | awk '{print $2;}') \
-  -i :0.0 \
+FILE_NAME=screen-record-$(date -u '+%Y-%m-%d-%H-%M-%S').mp4
+SLOP=$(slop -f "%x %y %w %h %g %i") || exit 1
+read -r X Y W H G ID <<< $SLOP
+
+ffmpeg -f x11grab \
+  -s "$W"x"$H" \
+  -i :0.0+$X,$Y \
   -framerate 60 \
   /home/calvin/Videos/screen-record/${FILE_NAME}
