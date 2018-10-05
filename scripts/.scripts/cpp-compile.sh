5b371df8266ff5fd9732e5759ba84e9892eaa9d1
Calvin Cieslak <calvin.cieslak@gmail.com>
2018-10-05 14:02:35 -0700

    Add inotifywait to cpp-compile script

--- scripts/.scripts/cpp-compile.sh
+++ scripts/.scripts/cpp-compile.sh
@@ -1,20 +1,38 @@
 # !/bin/sh
-SOURCE_FILES=()
+function compile_run() {
+  files=$1
 
-# Loop over files in current directory and push to array
-for i in $(pwd)/*; do
-  if [[ $i == *.cpp ]]; then
-    SOURCE_FILES=("${SOURCE_FILES[@]}" "$i")
-  fi
+  # Logging
+  echo "Compiling:"
+  for k in ${files[@]}; do
+    echo $k
+  done
+  echo "============================================="
+
+  # Compile source files and run
+  g++ -o prog ${files[@]} && ./prog
+}
+
+function find_files() {
+  SOURCE_FILES=()
+  # Loop over files in current directory and push to array
+  for i in $(pwd)/*; do
+    if [[ $i == *.cpp ]]; then
+      SOURCE_FILES=("${SOURCE_FILES[@]}" "$i")
+    fi
+  done
+  echo "Files in find_files: $SOURCE_FILES"
+}
+
+# Run once before the inotifywait loop
+find_files
+compile_run "$SOURCE_FILES"
+
+# Taken from https://exyr.org/2011/inotify-run/
+FORMAT=$(echo -e "\033[1;33m%w%f\033[0m written")
+while inotifywait -qre close_write --format "$FORMAT" .
+do
+  find_files
+  compile_run "$SOURCE_FILES"
 done
 
-# Logging
-echo "Compiling:"
-for k in ${SOURCE_FILES[@]}; do
-  echo $k
-done
-echo "============================================="
-printf "\n"
-
-# Compile source files and run
-g++ -o prog ${SOURCE_FILES[@]} && ./prog
