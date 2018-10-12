2e9f004b258f88437bfe913418e0045bebc6571c
Calvin Cieslak <calvin.cieslak@gmail.com>
2018-10-12 09:27:41 -0700

    Simplify lock script

--- i3/.config/i3/lock.sh
+++ i3/.config/i3/lock.sh
@@ -1,28 +1,8 @@
 #!/bin/bash
-
-# Take a screenshot:
-SCREENSHOT=/tmp/screen.png
-scrot $SCREENSHOT
-
-# Lock image and blur type variables
-LOCK_IMAGE=~/.config/i3/antergos.png
-BLURTYPE="0x4"
-
-# Create a blur on the shot:
-convert $SCREENSHOT -blur $BLURTYPE $SCREENSHOT
-
-# Find number of currently connected monitors
-NUMCONNECTED=$(xrandr | grep -w connected | wc -l)
-
-# Add the lock to the blurred image:
-if [ NUMCONNECTED -eq 2 ]; then
-  [[ -f $LOCK_IMAGE ]] && convert $SCREENSHOT $LOCK_IMAGE -gravity center -composite -matte $SCREENSHOT
-else
-  [[ -f $LOCK_IMAGE ]] && convert $SCREENSHOT $LOCK_IMAGE -gravity center -composite -matte $SCREENSHOT
-fi
+LOCK_IMAGE=~/.config/i3/screen.png
 
 # Pause music (mocp and mpd):
 playerctl pause
 
 # Lock it up!
-i3lock -u -e -f -c 000000 -i $SCREENSHOT
+i3lock -u -e -f -c 000000 -i $LOCK_IMAGE
