d0fa94df66014986975d9e59476de4fe718a1b4c
Calvin Cieslak <calvin.cieslak@gmail.com>
2018-10-14 10:30:03 -0700

    Tweak the fresh install script

--- fresh-install.sh
+++ fresh-install.sh
@@ -65,11 +65,8 @@ git clone https://aur.archlinux.org/spotify.git && cd spotify && makepkg -isr &&
 git clone https://aur.archlinux.org/snapd.git && cd snapd && makepkg -isr && cd ..
 git clone https://aur.archlinux.org/google-chrome.git && cd google-chrome && makepkg -isr && cd ..
 git clone https://aur.archlinux.org/paper-icon-theme.git && cd paper-icon-theme && makepkg -isr && cd ..
-git clone https://aur.archlinux.org/dropbox.git && cd dropbox && makepkg -si && cd ~
-
-# Install slack with snap instead of AUR
-sudo ln -s /var/lib/snapd/snap /snap
-sudo snap install slack --classic
+git clone https://aur.archlinux.org/menu-calc.git && cd menu-calc && makepkg -isr && cd ..
+git clone https://aur.archlinux.org/dropbox.git && cd dropbox && makepkg -isr && cd ~
 
 # Install oh-my-zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+# sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
