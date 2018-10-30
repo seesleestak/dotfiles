88b8f6ea215f7de8f3a2bc676d799e438b47d76d
Calvin Cieslak <calvin.cieslak@gmail.com>
2018-10-30 15:16:05 -0700

    Add case statement for slack script

--- scripts/.scripts/slack.sh
+++ scripts/.scripts/slack.sh
@@ -1,5 +1,13 @@
 # !/bin/sh
-google-chrome-stable --app="https://hixme.slack.com/messages"
-if [ $1 == '--all' ]; then
-  google-chrome-stable --app="https://hixme2.slack.com/messages"
-fi
+case $1 in
+  -a|--all)
+    google-chrome-stable --app="https://hixme.slack.com/messages"
+    google-chrome-stable --app="https://hixme2.slack.com/messages"
+    ;;
+  -2|--hixme2)
+    google-chrome-stable --app="https://hixme2.slack.com/messages"
+    ;;
+  *)
+    google-chrome-stable --app="https://hixme.slack.com/messages"
+    ;; 
+esac
