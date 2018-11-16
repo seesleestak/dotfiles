2d6fec1b3e206cfb947bd57550abe96add65aeea
Calvin Cieslak <calvin.cieslak@gmail.com>
2018-11-16 09:33:17 -0800

    Misc changes in quit-all

--- scripts/.scripts/quit-all.sh
+++ scripts/.scripts/quit-all.sh
@@ -1,13 +1,11 @@
 # !/bin/sh
 
 # Kill all commonly used apps
-killall slack
-killall chrome
-killall spotify
-killall urxvt
-killall telegram-desktop
-killall libreoffice
-killall gpicview
+killall -g chrome
+killall -g spotify
+killall -g urxvt
+killall -g telegram-desktop
+killall -g libreoffice
 
 # Go to first workspace
 i3-msg workspace "1"
