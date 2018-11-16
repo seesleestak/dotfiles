85b7376ef723be8a92c548135caa075c83aca534
Calvin Cieslak <calvin.cieslak@gmail.com>
2018-11-16 09:30:56 -0800

    Add rounded corners config

--- polybar/.config/polybar/scripts/toggle-notifications.sh
+++ polybar/.config/polybar/scripts/toggle-notifications.sh
@@ -4,7 +4,8 @@ STATE=$(ps -e | grep dunst | head -n 1)
 if [[ -n $STATE ]]; then
   notify-send "Silencing notifications..."
   sleep 2
-  killall dunst
+  notify-send DUNST_COMMAND_PAUSE
 else
+  notify-send DUNST_COMMAND_RESUME
   notify-send "Resuming notification service..."
 fi
