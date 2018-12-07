6ecaa418c2fcc8822b2e6c5409264fbc6731d2ff
Calvin Cieslak <calvin.cieslak@gmail.com>
2018-12-07 11:11:36 -0800

    Update json-format to format a JS object into JSON

--- scripts/.scripts/json-format.sh
+++ scripts/.scripts/json-format.sh
@@ -1,2 +1,15 @@
 # !/bin/sh
-xsel -ob | python -c 'import json, sys; print(json.dumps(json.load(sys.stdin), indent=2))' | xsel -ib
+case "$1" in
+  -j)
+    CONTENT=$(xsel -ob)
+
+    node -p "JSON.stringify($CONTENT)" | \
+    python -c 'import json, sys; print(json.dumps(json.load(sys.stdin), indent=2))' | \
+    xsel -ib
+    ;;
+  *)
+    xsel -ob | \
+    python -c 'import json, sys; print(json.dumps(json.load(sys.stdin), indent=2))' | \
+    xsel -ib
+    ;;
+esac
