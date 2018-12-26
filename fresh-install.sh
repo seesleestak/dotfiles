f2ec5165306e6da3232847077195b532ce66823c
Calvin Cieslak <calvin.cieslak@gmail.com>
2018-12-26 09:40:46 -0800

    Polybar updates for fresh install

--- fresh-install.sh
+++ fresh-install.sh
@@ -46,6 +46,7 @@ pacmanPreferredPrograms() {
     npm \
     ranger \
     ripgrep \
+    polybar \
     rofi \
     scrot \
     slop \
@@ -59,6 +60,9 @@ pacmanPreferredPrograms() {
     zathura-pdf-poppler \
     zathura-ps \
     zsh
+
+  # Change shell to zsh
+  # chsh -s $(which zsh)
 }
 
 pacmanSetup() {
@@ -81,7 +85,6 @@ pacmanSetup() {
 aurPackages() {
   # Clone some stuff from AUR
   mkdir -p ~/aur && cd ~/aur
-  git clone https://aur.archlinux.org/polybar.git && cd polybar && makepkg -isr && cd ..
   git clone https://aur.archlinux.org/spotify.git && cd spotify && makepkg -isr && cd ..
   git clone https://aur.archlinux.org/google-chrome.git && cd google-chrome && makepkg -isr && cd ..
   git clone https://aur.archlinux.org/paper-icon-theme.git && cd paper-icon-theme && makepkg -isr && cd ..
@@ -115,13 +118,13 @@ distroAgnosticSetup() {
     serverless
 
   # Create directories
-  mkdir ~/working
+  mkdir -p ~/working
   mkdir -p ~/Pictures/screenshots
   mkdir -p ~/Videos/screen-record
 
   # Install custom suckless builds
-  cd ~/working && git clone git@github.com:seesleestak/st.git && cd st && make && sudo make install
-  cd ~/working && git clone git@github.com:seesleestak/dmenu.git && cd dmenu && make && sudo make install
+  cd ~/working && git clone https://github.com/seesleestak/st.git && cd st && make && sudo make install
+  cd ~/working && git clone https://github.com/seesleestak/dmenu.git && cd dmenu && make && sudo make install
 
   # Install oh-my-zsh
   # sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
