93c5da3f6e03b8c2a443a6433f21a26d3c919fb1
Calvin Cieslak <calvin.cieslak@gmail.com>
2018-12-26 13:20:24 -0800

    Spawn vim instead of yad on clients script

--- scripts/.scripts/clients.sh
+++ scripts/.scripts/clients.sh
@@ -1,2 +1,3 @@
 # !/bin/sh
-yad --text-info --filename=/home/calvin/working/notes/misc/client-id --width=500 --height=400
+# yad --text-info --filename=/home/calvin/working/notes/misc/client-id --width=500 --height=400
+urxvt -e vim /home/calvin/working/notes/misc/client-id
