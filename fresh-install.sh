fc91015c49cbd7b8db06da18d573cda99d9bd5e7
Calvin Cieslak <calvin.cieslak@gmail.com>
2019-01-08 12:56:58 -0800

    Remove oh-my-zsh

--- fresh-install.sh
+++ fresh-install.sh
@@ -125,9 +125,6 @@ distroAgnosticSetup() {
   # Install custom suckless builds
   cd ~/working && git clone https://github.com/seesleestak/st.git && cd st && make && sudo make install
   cd ~/working && git clone https://github.com/seesleestak/dmenu.git && cd dmenu && make && sudo make install
-
-  # Install oh-my-zsh
-  # sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 }
 
 case $1 in
