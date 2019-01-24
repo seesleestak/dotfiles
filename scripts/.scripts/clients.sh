c5c1a277df2b7f63ae4c4d3fe2c3ca21890b139a
Calvin Cieslak <calvin.cieslak@gmail.com>
2019-01-24 12:42:24 -0800

    Remove 'working' ref in clients script

--- scripts/.scripts/clients.sh
+++ scripts/.scripts/clients.sh
@@ -1,3 +1,2 @@
 # !/bin/sh
-# yad --text-info --filename=/home/calvin/working/notes/misc/client-id --width=500 --height=400
-urxvt -title scratch -e vim /home/calvin/working/notes/misc/client-id
+urxvt -title scratch -e vim /home/calvin/repos/notes/hixme/client-id
