da2a007a36fcf50d7fb727c743cfae67f9be5ac4
Calvin Cieslak <calvin.cieslak@gmail.com>
2019-01-24 12:42:56 -0800

    Customize i3lock

--- i3/.config/i3/lock.sh
+++ i3/.config/i3/lock.sh
@@ -9,4 +9,21 @@ convert $WALLPAPER $LOCK_IMAGE -gravity center -composite -matte $SCREENSHOT
 playerctl pause
 
 # Lock it up!
-i3lock -e -f -c 000000 -i $SCREENSHOT -t
+i3lock \
+  -e \
+  -f \
+  -c 000000 \
+  -i $SCREENSHOT \
+  -t \
+  --clock \
+  --timepos="960:740" \
+  --time-font="DejaVu Sans Mono" \
+  --timesize=42 \
+  --datesize=20 \
+  --radius 140 \
+  --ring-width 10 \
+  --veriftext="" \
+  --insidevercolor=00000000 \
+  --noinputtext="" \
+  --insidecolor=00000000 \
+  --insidewrongcolor=00000000
