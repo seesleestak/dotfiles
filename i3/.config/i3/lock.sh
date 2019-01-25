37d0022feccd9db7917be26e785c8c365633ba08
Calvin Cieslak <calvin.cieslak@gmail.com>
2019-01-25 14:45:49 -0800

    Add lock-image, i3lock tweaks

--- i3/.config/i3/lock.sh
+++ i3/.config/i3/lock.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-SCREENSHOT=/tmp/blurwall.png
-WALLPAPER=~/wallpaper/plant-resize.png
-LOCK_IMAGE=~/.config/i3/lock-icon.png
+# SCREENSHOT=/tmp/blurwall.png
+# WALLPAPER=~/wallpaper/plant-resize.png
+LOCK_IMAGE=~/.config/i3/lock-image.png
 
-convert $WALLPAPER $LOCK_IMAGE -gravity center -composite -matte $SCREENSHOT
+# convert $WALLPAPER $LOCK_IMAGE -gravity center -composite -matte $SCREENSHOT
 
 # Pause music
 playerctl pause
@@ -13,17 +13,22 @@ i3lock \
   -e \
   -f \
   -c 000000 \
-  -i $SCREENSHOT \
+  -i $LOCK_IMAGE \
   -t \
   --clock \
-  --timepos="960:740" \
-  --time-font="DejaVu Sans Mono" \
+  --timepos="ix:iy+210" \
+  --timestr="%I:%M:%S %p" \
+  --time-font="Roboto" \
   --timesize=42 \
-  --datesize=20 \
-  --radius 140 \
+  --timecolor=E5E9F0FF \
+  --datesize=24 \
+  --datecolor=4C566AFF \
+  --date-font="Roboto" \
+  --radius 135 \
   --ring-width 10 \
   --veriftext="" \
   --insidevercolor=00000000 \
   --noinputtext="" \
+  --wrongtext="incorrect" \
   --insidecolor=00000000 \
   --insidewrongcolor=00000000
