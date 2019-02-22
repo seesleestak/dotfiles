99feb7d3598980e7afe76b9adc81d180f8467e57
Calvin Cieslak <calvin.cieslak@gmail.com>
2019-02-21 19:18:06 -0800

    Add gif conversion to screen-record options

---
+++ scripts/.scripts/mp4-to-gif.sh
@@ -0,0 +1,11 @@
+# !/bin/sh
+EXTENSIONLESS=$(echo $1 | cut -d\. -f1)
+
+ffmpeg -y \
+  -i $1 \
+  -vf fps=10,scale=860:-1:flags=lanczos,palettegen palette.png
+
+ffmpeg -y -i $1 -i palette.png -filter_complex \
+  "fps=10,scale=860:-1:flags=lanczos[x];[x][1:v]paletteuse" ${EXTENSIONLESS}.gif
+
+rm palette.png
--- scripts/.scripts/screen-record.sh
+++ scripts/.scripts/screen-record.sh
@@ -1,5 +1,6 @@
 # !/bin/bash
-FILE_NAME=screen-record-$(date -u '+%Y-%m-%d-%H-%M-%S').mp4
+FILE_NAME=screen-record-$(date -u '+%Y-%m-%d-%H-%M-%S')
+BASE_PATH=/home/calvin/Videos/screen-record
 SLOP=$(slop -f "%x %y %w %h") || exit 1
 read -r X Y W H <<< $SLOP
 
@@ -8,11 +9,30 @@ echo "Y: $Y"
 echo "W: $W"
 echo "H: $H"
 
-# https://stackoverflow.com/questions/20847674/ffmpeg-libx264-height-not-divisible-by-2
-ffmpeg -f x11grab \
-  -s "$W"x"$H" \
-  -i :0.0+$X,$Y \
-  -vcodec h264 \
-  -pix_fmt yuv420p \
-  -vf "pad=ceil(iw/2)*2:ceil(ih/2)*2" \
-  /home/calvin/Videos/screen-record/${FILE_NAME}
+function mp4Screengrab {
+  # https://stackoverflow.com/questions/20847674/ffmpeg-libx264-height-not-divisible-by-2
+  ffmpeg -f x11grab \
+    -s "$W"x"$H" \
+    -i :0.0+$X,$Y \
+    -vcodec h264 \
+    -pix_fmt yuv420p \
+    -vf "pad=ceil(iw/2)*2:ceil(ih/2)*2" \
+    ${BASE_PATH}/${FILE_NAME}.mp4
+}
+
+function convertToGif {
+  notify-send 'Screen capture' 'Converting to gif...'
+  mp4-to-gif.sh ${BASE_PATH}/${FILE_NAME}.mp4
+  mv ${BASE_PATH}/${FILE_NAME}.gif ${BASE_PATH}/gif/${FILE_NAME}.gif
+  notify-send 'Screen capture' 'Complete'
+}
+
+case $1 in
+  -g|--gif)
+    mp4Screengrab
+    convertToGif
+    ;;
+  *)
+    mp4Screengrab
+    ;;
+esac
