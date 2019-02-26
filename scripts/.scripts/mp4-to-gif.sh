146da087353be5fe36392758f52f24d8e6184d61
Calvin Cieslak <calvin.cieslak@gmail.com>
2019-02-26 09:52:02 -0800

    Up the scale on mp4-to-gif script

--- scripts/.scripts/mp4-to-gif.sh
+++ scripts/.scripts/mp4-to-gif.sh
@@ -3,9 +3,9 @@ EXTENSIONLESS=$(echo $1 | cut -d\. -f1)
 
 ffmpeg -y \
   -i $1 \
-  -vf fps=10,scale=860:-1:flags=lanczos,palettegen palette.png
+  -vf fps=10,scale=1000:-1:flags=lanczos,palettegen palette.png
 
 ffmpeg -y -i $1 -i palette.png -filter_complex \
-  "fps=10,scale=860:-1:flags=lanczos[x];[x][1:v]paletteuse" ${EXTENSIONLESS}.gif
+  "fps=10,scale=1000:-1:flags=lanczos[x];[x][1:v]paletteuse" ${EXTENSIONLESS}.gif
 
 rm palette.png
