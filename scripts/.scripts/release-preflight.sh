e531b4954909a3e5d873787a54966bd717fbae0d
Calvin Cieslak <calvin.cieslak@gmail.com>
2019-03-07 08:59:56 -0800

    Consolidate grep options

--- scripts/.scripts/release-preflight.sh
+++ scripts/.scripts/release-preflight.sh
@@ -11,9 +11,9 @@ CONVENTIONAL=()
 while read -r commit ; do
   if echo $commit | grep -q "release/*"; then
     continue
-  elif echo $commit | grep -q -e "^[^:]*$"; then 
+  elif echo $commit | grep -qE "^[^:]*$"; then 
     OTHER=("${OTHER[@]}" "$(echo $commit | cut -c 10-)")
-  elif echo $commit | grep -q -E "[A-Za-z]{3,11}\/[A-Za-z]{2,4}-[0-9]{1,5}"; then
+  elif echo $commit | grep -qE "[A-Za-z]{3,11}\/[A-Za-z]{2,4}-[0-9]{1,5}"; then
     CONVENTIONAL=("${CONVENTIONAL[@]}" "$(echo $commit | cut -c 10- | cut -d ":" -f 1)")
   else
     OTHER=("${OTHER[@]}" "$(echo $commit | cut -c 10- | cut -d ":" -f 1)")
