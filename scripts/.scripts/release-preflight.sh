5452296cf5cfcd6c7056ff0c51e1f9bd4af67a04
Calvin Cieslak <calvin.cieslak@gmail.com>
2019-03-06 19:20:03 -0800

    Use bash arrays to store other commits

--- scripts/.scripts/release-preflight.sh
+++ scripts/.scripts/release-preflight.sh
@@ -1,14 +1,27 @@
 # Get latest
+IFS=$'\n'
+TMP_FILE=/tmp/release.log
 git fetch origin master develop -q
 
 LOG="git log master..develop --oneline --no-merges --no-decorate"
 
-$LOG | while read -r commit ; do
+git log master..develop --oneline --no-merges --no-decorate > /tmp/release.log
+
+OTHER=()
+CONVENTIONAL=()
+while read -r commit ; do
   if echo $commit | grep -q -e "^[^:]*$"; then 
-    echo $commit | cut -c 10-
-  else
-    echo $commit | cut -c 10- | cut -d ":" -f 1
+    OTHER=("${OTHER[@]}" "$(echo $commit | cut -c 10-)")
+  elif echo $commit | grep -q -v "release/*"; then
+    CONVENTIONAL=("${CONVENTIONAL[@]}" "$(echo $commit | cut -c 10- | cut -d ":" -f 1)")
   fi
-done | \
-sort -u | \
-grep -v "release/*"
+done < /tmp/release.log
+
+echo "TICKETS ===>"
+sort -u <<<"${CONVENTIONAL[*]}"
+
+echo "OTHER ===>"
+echo "${OTHER[*]}"
+
+rm $TMP_FILE
+unset IFS
