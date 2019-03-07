a926e947e58d18f9149e9e53acb2785cb1d1633b
Calvin Cieslak <calvin.cieslak@gmail.com>
2019-03-06 18:51:14 -0800

    Add handling for commits that don't follow the convention

--- scripts/.scripts/release-preflight.sh
+++ scripts/.scripts/release-preflight.sh
@@ -3,9 +3,12 @@ git fetch origin master develop -q
 
 LOG="git log master..develop --oneline --no-merges --no-decorate"
 
-$LOG | \
-while read -r commit ; do
-  echo $commit | cut -d " " -f 2 | cut -d ":" -f 1
+$LOG | while read -r commit ; do
+  if echo $commit | grep -q -e "^[^:]*$"; then 
+    echo $commit | cut -c 10-
+  else
+    echo $commit | cut -c 10- | cut -d ":" -f 1
+  fi
 done | \
 sort -u | \
 grep -v "release/*"
