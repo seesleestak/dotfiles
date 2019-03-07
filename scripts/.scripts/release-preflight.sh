c656aa134876b98423ed40feececde1935120f76
Calvin Cieslak <calvin.cieslak@gmail.com>
2019-03-07 08:50:58 -0800

    Move release case earlier, sort unique for other as well

--- scripts/.scripts/release-preflight.sh
+++ scripts/.scripts/release-preflight.sh
@@ -9,12 +9,12 @@ git log master..develop --oneline --no-merges --no-decorate > $TMP_FILE
 OTHER=()
 CONVENTIONAL=()
 while read -r commit ; do
-  if echo $commit | grep -q -e "^[^:]*$"; then 
+  if echo $commit | grep -q "release/*"; then
+    continue
+  elif echo $commit | grep -q -e "^[^:]*$"; then 
     OTHER=("${OTHER[@]}" "$(echo $commit | cut -c 10-)")
   elif echo $commit | grep -q -E "[A-Za-z]{3,11}\/[A-Za-z]{2,4}-[0-9]{1,5}"; then
     CONVENTIONAL=("${CONVENTIONAL[@]}" "$(echo $commit | cut -c 10- | cut -d ":" -f 1)")
-  elif echo $commit | grep -q "release/*"; then
-    continue
   else
     OTHER=("${OTHER[@]}" "$(echo $commit | cut -c 10- | cut -d ":" -f 1)")
   fi
@@ -24,7 +24,7 @@ echo "======= TICKETS ======="
 sort -u <<<"${CONVENTIONAL[*]}"
 
 echo "======= OTHER ======="
-echo "${OTHER[*]}"
+sort -u <<<"${OTHER[*]}"
 
 rm $TMP_FILE
 unset IFS
