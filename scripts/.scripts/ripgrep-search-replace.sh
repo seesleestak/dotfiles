bf0c590966ab1972c4ec10ab672cae1afa4edda3
Calvin Cieslak <calvin.cieslak@gmail.com>
2019-08-11 13:22:05 -0700

    Add rsr and vimstart to scripts

---
+++ scripts/.scripts/ripgrep-search-replace.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+if [[ -n $1 ]] && [[ -n $2 ]]; then
+  COUNT=$(rg -l $1 | wc -l | xargs)
+  if [[ $COUNT -gt 0 ]]; then
+    rg $1 --replace $2
+    echo "\n\033[0;33m$COUNT matches found"
+    echo -n "\033[0;36mContinue? (y/n) \033[0;39m" 
+    read ANSWER
+    case $ANSWER in
+      y|Y) 
+        rg -l $1 | xargs sed -i '' "s|$1|$2|g"
+        echo "\033[0;32mSuccessfully made $COUNT substitutions"
+        ;;
+      n|N|*) ;;
+    esac
+  else
+    echo "\033[0;33mNo matches found"
+  fi
+else 
+  echo "\033[0;31mNot enough arguments"
+fi
---
+++ scripts/.scripts/vimstart.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# vimstart - by Billy Montgomery
+PURPLE="\033[0;35m"
+ORANGE="\033[0;33m"
+
+echo $ORANGE
+TOTAL=0
+for i in {1..20}
+do
+  vim -c\ q --startuptime /tmp/vim.log
+  VAL=$(tail -n1 /tmp/vim.log | awk '{print $1}')
+  echo $VAL
+  TOTAL=$(echo $VAL | awk '{print $1+"'$TOTAL'"}')
+done
+
+echo $PURPLE
+echo $TOTAL | awk '{print $1/20 " Average \n"}'
