c6dcdf3b8b4322edcbe7cc5a0821115e9820f0fc
Calvin Cieslak <calvin.cieslak@gmail.com>
2019-09-25 08:37:21 -0700

    Remove exit message, switch up conditional blocks

--- scripts/.scripts/branch-prefix.sh
+++ scripts/.scripts/branch-prefix.sh
@@ -1,12 +1,10 @@
 # !/bin/bash
 CURR_BRANCH=$(git symbolic-ref HEAD --short)
-if [[ -z $CURR_BRANCH ]]; then
-  echo "No current branch. Exiting..."
-  exit
-fi
 
-if [[ $CURR_BRANCH != "master" ]] || [[ $CURR_BRANCH != "develop" ]]; then
-  git commit -m "$CURR_BRANCH: $1"
-else
-  git commit -m $1
+if [[ -n $CURR_BRANCH ]]; then
+  if [[ $CURR_BRANCH != "master" ]] || [[ $CURR_BRANCH != "develop" ]]; then
+    git commit -m "$CURR_BRANCH: $1"
+  else
+    git commit -m $1
+  fi
 fi
