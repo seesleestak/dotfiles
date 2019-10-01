dd7a36e44244375e535140d5bc34446940bc8766
Calvin Cieslak <calvin.cieslak@gmail.com>
2019-09-30 17:09:25 -0700

    Fix branch-prefix script

--- scripts/.scripts/branch-prefix.sh
+++ scripts/.scripts/branch-prefix.sh
@@ -2,9 +2,10 @@
 CURR_BRANCH=$(git symbolic-ref HEAD --short)
 
 if [[ -n $CURR_BRANCH ]]; then
-  if [[ $CURR_BRANCH != "master" ]] || [[ $CURR_BRANCH != "develop" ]]; then
-    git commit -m "$CURR_BRANCH: $1" "${@:2}"
-  else
+  echo "CURR_BRANCH --- $CURR_BRANCH"
+  if [[ "$CURR_BRANCH" == "master" ]] || [[ "$CURR_BRANCH" == "develop" ]]; then
     git commit -m $1 "${@:2}"
+  else
+    git commit -m "$CURR_BRANCH: $1" "${@:2}"
   fi
 fi
