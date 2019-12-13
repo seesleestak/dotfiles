5c91b2b78180907478ff43071555c5d249987011
Calvin Cieslak <calvin.cieslak@gmail.com>
2019-12-13 07:52:46 -0800

    Use pbcopy/pbpaste in mac-os json-format script

--- scripts/.scripts/json-format.sh
+++ scripts/.scripts/json-format.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 case "$1" in
   -j)
-    CONTENT=$(xsel -ob)
+    CONTENT=$(pbpaste)
     node -p "JSON.stringify($CONTENT)" | \
     python -c 'import json, sys; print(json.dumps(json.load(sys.stdin), indent=2))' | \
-    xsel -ib
+    pbcopy
     ;;
   *)
-    xsel -ob | \
+    pbpaste | \
     python -c 'import json, sys; print(json.dumps(json.load(sys.stdin), indent=2))' | \
-    xsel -ib
+    pbcopy
     ;;
 esac
