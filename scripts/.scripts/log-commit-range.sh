c81e78a44da0acfd4d79e04afc84bf839ac2ef17
Calvin Cieslak <calvin.cieslak@gmail.com>
2020-01-25 20:46:01 -0800

    Check number for log-commit-range

--- scripts/.scripts/log-commit-range.sh
+++ scripts/.scripts/log-commit-range.sh
@@ -1,7 +1,7 @@
 # !/bin/bash
 NUM_COMMITS=$1
 
-if [[ -n $NUM_COMMITS ]]; then
+if [[ -n $NUM_COMMITS ]] && [[ $NUM_COMMITS > 0 ]] ; then
   git log --oneline "HEAD~$NUM_COMMITS"..HEAD
 else
   git log --oneline
