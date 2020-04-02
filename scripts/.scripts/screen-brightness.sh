40240ae6bdc6a51c4d5d507200d166d532048541
Calvin Cieslak <calvin.cieslak@gmail.com>
2020-04-02 06:46:06 -0700

    Use synchronous hint for brightness script

--- scripts/.scripts/screen-brightness.sh
+++ scripts/.scripts/screen-brightness.sh
@@ -11,8 +11,8 @@ if [[ -n $1 ]]; then
   NEW_PERCENT=$(xbacklight -get)
 
   if [[ $NEW_PERCENT != $CURR_PERCENT ]]; then
-    CURR_PERCENT=$(xbacklight -get)
-    notify-send "brightness $CURR_PERCENT%"
+    CURR_PERCENT=$(xbacklight -get | cut -d "." -f 1)
+    notify-send "Brightness $CURR_PERCENT%" --app-name=brightness --hint=string:synchronous:brightness
   fi
 else
   echo "Please provide an argument, 0 or 1"
