42c0b031aa7d915dcb34b6bef134fe9dc3443642
Calvin Cieslak <calvin.cieslak@gmail.com>
2020-05-26 09:23:18 -0700

    Pair down ah-branch-prefix script

--- scripts/.scripts/ah-branch-prefix.sh
+++ scripts/.scripts/ah-branch-prefix.sh
@@ -1,16 +1,13 @@
 # !/bin/bash
 CURR_BRANCH=$(git symbolic-ref HEAD --short)
-BLACKLIST=("master" "develop")
-PREFIX=""
 
 if [[ -n $CURR_BRANCH ]]; then
-  if [[ -n $JIRA_PREFIX ]]; then
-    PREFIX=$(echo $CURR_BRANCH | grep -o -e "$JIRA_PREFIX-[0-9]*")
-  fi
+  PREFIX=$(echo $CURR_BRANCH | grep -o -e "[a-zA-Z]*-[0-9]*")
 
-  if [[ -z $PREFIX ]] || [[ $(echo "${BLACKLIST[@]}" | grep --word-regexp "$CURR_BRANCH") ]]; then
-    git commit -m $1 "${@:2}"
+  if [[ -n $PREFIX ]]; then
+    FORMATTED_PREFIX=$(echo $PREFIX | awk '{print toupper($0)}')
+    git commit -m "$FORMATTED_PREFIX: $1" "${@:2}"
   else
-    git commit -m "$PREFIX: $1" "${@:2}"
+    git commit -m "$1" "${@:2}"
   fi
 fi
