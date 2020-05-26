f78931b0efc06b9015dca108bf0c06368afe4024
Calvin Cieslak <calvin.cieslak@gmail.com>
2020-05-26 09:38:10 -0700

    Add some capture groups for prefix regex

--- scripts/.scripts/ah-branch-prefix.sh
+++ scripts/.scripts/ah-branch-prefix.sh
@@ -2,7 +2,7 @@
 CURR_BRANCH=$(git symbolic-ref HEAD --short)
 
 if [[ -n $CURR_BRANCH ]]; then
-  PREFIX=$(echo $CURR_BRANCH | grep -o -e "[a-zA-Z]*-[0-9]*")
+  PREFIX=$(echo $CURR_BRANCH | grep -o -E "([a-zA-Z]+)-([0-9]+)")
 
   if [[ -n $PREFIX ]]; then
     FORMATTED_PREFIX=$(echo $PREFIX | awk '{print toupper($0)}')
