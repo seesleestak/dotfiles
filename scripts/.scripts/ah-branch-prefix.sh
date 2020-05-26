7c51307d826e25e0c79d322164e4d0b6fa87a23b
Calvin Cieslak <calvin.cieslak@gmail.com>
2020-05-26 10:02:14 -0700

    Add error handling for no commit message

--- scripts/.scripts/ah-branch-prefix.sh
+++ scripts/.scripts/ah-branch-prefix.sh
@@ -4,10 +4,14 @@ CURR_BRANCH=$(git symbolic-ref HEAD --short)
 if [[ -n $CURR_BRANCH ]]; then
   PREFIX=$(echo $CURR_BRANCH | grep -o -E "([a-zA-Z]+)-([0-9]+)")
 
-  if [[ -n $PREFIX ]]; then
-    FORMATTED_PREFIX=$(echo $PREFIX | awk '{print toupper($0)}')
-    git commit -m "$FORMATTED_PREFIX: $1" "${@:2}"
+  if [[ -n $1 ]]; then
+    if [[ -n $PREFIX ]]; then
+      FORMATTED_PREFIX=$(echo $PREFIX | awk '{print toupper($0)}')
+      git commit -m "$FORMATTED_PREFIX: $1" "${@:2}"
+    else
+      git commit -m "$1" "${@:2}"
+    fi
   else
-    git commit -m "$1" "${@:2}"
+    echo 'Please enter a commit message'
   fi
 fi
