5f0bd0fe5b9abc3903e1a6a712b4333639c0ec56
Calvin Cieslak <calvin.cieslak@gmail.com>
2020-07-26 07:06:36 -0700

    Add check for log path and doc path in standup-update

--- scripts/.scripts/standup-update.sh
+++ scripts/.scripts/standup-update.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
-DOC_PATH="$HOME/log/standup.md"
+LOG_PATH="$HOME/log"
+DOC_PATH="$LOG_PATH/standup.md"
+if [ ! -e "$DOC_PATH" ]; then
+  mkdir -p $LOG_PATH
+  touch $DOC_PATH
+fi
+
 CURR_DATE=$(date "+%a %m-%d-%Y")
 GREP_RES=$(grep "$CURR_DATE" $DOC_PATH)
 if [ -z "$GREP_RES" ]; then
