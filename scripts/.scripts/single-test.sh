c65ea7528b823b6db14c2c1a7d841213f14e9c62
Calvin Cieslak <calvin.cieslak@adhocteam.us>
2020-07-27 17:54:19 -0700

    Update single-test script

--- scripts/.scripts/single-test.sh
+++ scripts/.scripts/single-test.sh
@@ -1,11 +1,28 @@
 #!/usr/bin/env bash
-FILE_TEST_PATH=$(fd -e js -E "*.test.js" -E "*.style.js" | fzf)
-FILE_TEST_NAME=$(basename $FILE_TEST_PATH | cut -f1 -d'.')
-TEST_FILE=$(fd $FILE_TEST_NAME -e "test.js" --max-results 1)
+FILE_SOURCE_PATH=$(fd -e js -E "*.test.js" -E "*.style.js" | fzf)
+FILE_SOURCE_NAME=$(echo $FILE_SOURCE_PATH | cut -f1 -d'.')
+
+TEST_FILE=$(fd $FILE_SOURCE_NAME -e "test.js" --max-results 1)
+
+# Look for test named after directory if it's the index
+if [ -z $TEST_FILE ] && [ "$(basename $FILE_SOURCE_PATH)" == "index.js" ]; then
+  FILE_SOURCE_DIRNAME=$(dirname $FILE_SOURCE_PATH)
+  TEST_FILE=$(fd --full-path "$FILE_SOURCE_DIRNAME/${FILE_SOURCE_DIRNAME##*/}.test.js")
+fi
 
 if [ -z $TEST_FILE ]; then
-  echo "No test file found"
+  echo -n "Search for test file? (y/n) "
+  read
+  if [ "$REPLY" == "y" ]; then
+    TEST_FILE=$(fd -e "test.js" -E "*.style.js" | fzf)
+  else
+    echo "Exiting..."
+  fi
+fi
+
+if [ -z $TEST_FILE ]; then
+  echo "No test file found, exiting..."
   exit
 fi
 
-npx jest $TEST_FILE --watch --coverage --collectCoverageFrom $FILE_TEST_PATH
+npx jest $TEST_FILE --watch --coverage --collectCoverageFrom $FILE_SOURCE_PATH
