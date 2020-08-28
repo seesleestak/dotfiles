7b018cae3951997a3df99a42ef12b091647a2f87
Calvin Cieslak <calvin.cieslak@adhocteam.us>
2020-08-28 14:57:11 -0700

    Simplify json-format

--- scripts/.scripts/json-format.sh
+++ scripts/.scripts/json-format.sh
@@ -1,14 +1,2 @@
 #!/usr/bin/env bash
-case "$1" in
-  -j)
-    CONTENT=$(pbpaste)
-    node -p "JSON.stringify($CONTENT)" | \
-    python -c 'import json, sys; print(json.dumps(json.load(sys.stdin), indent=2))' | \
-    pbcopy
-    ;;
-  *)
-    pbpaste | \
-    python -c 'import json, sys; print(json.dumps(json.load(sys.stdin), indent=2))' | \
-    pbcopy
-    ;;
-esac
+pbpaste | python -c 'import json, sys; print(json.dumps(json.load(sys.stdin), indent=2))' | pbcopy
