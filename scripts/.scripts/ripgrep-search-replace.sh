4d5fec413a8faf11f296e69621ba5589a9a2f2e2
Calvin Cieslak <calvin.cieslak@adhocteam.us>
2020-12-27 07:56:20 -0800

    Modify color handling in rsr script

--- scripts/.scripts/ripgrep-search-replace.sh
+++ scripts/.scripts/ripgrep-search-replace.sh
@@ -3,8 +3,8 @@ if [[ -n $1 ]] && [[ -n $2 ]]; then
   COUNT=$(rg $1 --count-matches | cut -d ':' -f 2 | awk '{s+=$1}END{print s}')
   if [[ $COUNT -gt 0 ]]; then
     rg $1 --replace $2
-    echo "\n\033[0;33m$COUNT matches found"
-    echo -n "\033[0;36mContinue? (y/n) \033[0;39m" 
+    echo -e "\n\e[33m$COUNT matches found"
+    echo -e -n "\e[36mContinue? (y/n) \e[39m" 
     read ANSWER
     case $ANSWER in
       y|Y) 
@@ -13,15 +13,15 @@ if [[ -n $1 ]] && [[ -n $2 ]]; then
         else
           rg -l $1 | xargs sed -i'' "s|$1|$2|g"
         fi
-        echo "\033[0;32mSuccessfully made $COUNT substitutions"
+        echo -e "\e[32mSuccessfully made $COUNT substitutions"
         ;;
       n|N|*) 
         echo "Exiting..."
         ;;
     esac
   else
-    echo "\033[0;33mNo matches found"
+    echo -e "\e[33mNo matches found"
   fi
 else 
-  echo "\033[0;31mNot enough arguments"
+  echo -e "\e[31mNot enough arguments"
 fi
